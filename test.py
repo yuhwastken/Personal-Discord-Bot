f48267a5e45b834ce273f1a6932799969f64aec4
YuhWasTken <92817057+yuhwastken@users.noreply.github.com>
2025-11-22 18:35:50 +0700

    Bind link inside message, handle wrong given url
    !bind "text" <link>

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
-import discord
-from discord.ui import Button, View
-from discord.ext import commands
-import requests
 import os
-import webserver
+from urllib.parse import urlparse
+
+import discord
+import requests
+from discord.ext import commands
+from discord.ui import Button, View
 from pytube import Playlist, YouTube
+
+import webserver
 
 DISCORD_API = os.environ["discordapi"]
 TENOR_API = os.environ["tenorapi"]
 STUDY_CHANNEL = os.evironment["study_channel"]
 GIF_CHANNEL = os.evironment["gif_channel"]
+
+
+def check_correct_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
 # Bot
 bot = commands.Bot(
     command_prefix="!",
@@ -56,40 +69,50 @@ async def gif(ctx, *, msg):
         message = await channel.send(embed=embed, view=view)
     else:
         await ctx.send(f"No gifs found for {msg}")
+    await ctx.message.delete()
 
 
 @bot.command()
-async def pl(ctx, url: str, utitle: str):
+async def pl(ctx, utitle: str, url: str):
     pl_url = "https://youtube.com/playlist?list=PL"
     yt1_url = "https://www.youtube.com/watch?v"
     yt2_url = "https://youtu.be/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        if url[:36] == pl_url:
-            playlist = Playlist(url)
-            links = playlist.video_urls
-            c = 1
-            des = ""
-            for link in links:
-                des += f"[Video Number {c}](<{link}>)" + " \n"
-                c += 1
-                if c == len(links):
-                    break
-            des += f"[Video Number {c}](<{links[-1]}>)"
-            embed = discord.Embed(
-                title=utitle,
-                description=des,
-            )
-            channel = bot.get_channel(STUDY_CHANNEL)
-            await channel.send(embed=embed)
-        elif url[:31] == yt1_url or url[:17] == yt2_url:
-            await ctx.send(f"[{utitle}](<{url}>)")
+    if check_correct_url(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            if url[:36] == pl_url:
+                playlist = Playlist(url)
+                links = playlist.video_urls
+                c = 1
+                des = ""
+                for link in links:
+                    des += f"[Video Number {c}](<{link}>)" + " \n"
+                    c += 1
+                    if c == len(links):
+                        break
+                des += f"[Video Number {c}](<{links[-1]}>)"
+                embed = discord.Embed(
+                    title=utitle,
+                    description=des,
+                )
+                channel = bot.get_channel(STUDY_CHANNEL)
+                await channel.send(embed=embed)
+            elif url[:31] == yt1_url or url[:17] == yt2_url:
+                await ctx.send(f"[{utitle}](<{url}>)")
+            await ctx.message.delete()
     else:
         await ctx.send("Your given link is wrong")
 
 
+@bot.command()
+async def bind(ctx, utitle: str, url: str):
+    if check_correct_url(url):
+        channel_id = ctx.channel.id
+        channel = bot.get_channel(channel_id)
+        await channel.send(f"[{utitle}](<{url}>)")
+    else:
+        await ctx.message.delete()
+
+
 webserver.keep_alive()
 bot.run(DISCORD_API)
-
-
-
